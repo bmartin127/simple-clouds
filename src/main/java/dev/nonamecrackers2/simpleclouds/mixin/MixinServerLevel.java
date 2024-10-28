d63a03e34592a52c2d4bdf2d4d8368dd8b54a29b
bmartin127 <minecraft.bradley.martin@gmail.com>
2024-10-28 17:37:34 -0600

    Fixed Cauldrons And Snow Accumulation

--- src/main/java/dev/nonamecrackers2/simpleclouds/mixin/MixinServerLevel.java
+++ src/main/java/dev/nonamecrackers2/simpleclouds/mixin/MixinServerLevel.java
@@ -1,9 +1,9 @@
 package dev.nonamecrackers2.simpleclouds.mixin;
 
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import dev.nonamecrackers2.simpleclouds.common.world.CloudManager;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.LevelChunk;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,6 +15,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.storage.DimensionDataStorage;
+
+import static dev.nonamecrackers2.simpleclouds.common.event.TickChunks.RainAndSnowVanillaCompatibility;
 
 @Mixin(ServerLevel.class)
 public abstract class MixinServerLevel implements CloudManagerAccessor<ServerLevel>
@@ -40,6 +42,14 @@ public abstract class MixinServerLevel implements CloudManagerAccessor<ServerLev
 		{
 			this.resetWeatherCycle();
 			ci.cancel();
+		}
+	}
+	@Inject(method = "tickChunk", at = @At(value = "RETURN"))
+	public void simpleclouds$LocalizedWeatherHandlePrecipitation(LevelChunk chunk, int tickSpeed, CallbackInfo ci)
+	{
+		CloudManager<?> manager = CloudManager.get((Level)(Object)this);
+		if (!manager.shouldUseVanillaWeather()){
+			RainAndSnowVanillaCompatibility((ServerLevel) (Object) this, chunk);
 		}
 	}
 	
